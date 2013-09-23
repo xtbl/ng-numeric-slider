b28aef20d173ae0d9c9dafb049911ffdad9f416e
cavila <cavila@IT-DSK-597.gap.com>
2013-09-23 10:50:06 -0600

    e2e tests added

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 		autoWatch: false,
 		browsers: ['Chrome'],
 		singleRun: false,
-        autoWatch: true,
+        autoWatch: false,
         urlRoot: '/_karma_/',
 		proxies: {
 			'/': 'http://localhost:8000/'
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,37 +2,63 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('angular flip-switch', function() {
+describe('angular numeric-slider', function() {
 
   beforeEach(function() {
     browser().navigateTo('../../app/index.html');
   });
 
-    it('should render the flip-switch directive', function() {
-        expect(element('select').html()).toBeDefined();
+    // render the slider
+    // click on slider changes the value
+    //
+    // click on current value label turn it editable
+    // mouse over on current value label cause a highlight
+    // add value to current value label
+    // Alphabetic entries in the value text field should be prohibited
+    // numbers that are either too large or too small should ‘snap’ to the closest value when the user finishes editing
+
+
+    it('should render the slider directive', function() {
+        expect(element('a.ui-slider-handle').html()).toBeDefined();
     });
 
-    it('should render flip-switch options', function() {
-        console.log('select is: ' + element('select').html());
-        expect(element('select').html()).toContain('ON');
-        expect(element('select').html()).toContain('OFF');
+    it('should render slider min and max', function() {
+        console.log('slider min is: ' + element('input[type="range"]').attr('min'));
+        console.log('slider max is: ' + element('input[type="range"]').attr('max'));
+        expect(element('input[type=range]').attr('min')).toBe('0');
+        expect(element('input[type=range]').attr('max')).toBe('100');
     });
 
-    it('should display the flip-switch label', function() {
-        expect(element('label').html()).toContain('Flip Switch Label');
+    it('should display the current value label', function() {
+        expect(element('.current-value-label').html()).toContain('Current Value Label');
     });
 
-    it('should slide the switch state when "ON" is selected', function() {
-        select('flipSwitchSelect').option('on');
-        expect(element('.ui-slider-label-a').attr('style')).toBe('width: 100%;');
-        expect(element('.ui-slider-label-b').attr('style')).toBe('width: 0%;');
+    it('click on current value label should turn it editable', function() {
+        element('.current-value-label').click(); // maybe could use model
+        expect(element('.current-value-label').html()).toNotContain('readonly');
     });
 
-    it('should slide the switch state when "OFF" is selected', function() {
-        select('flipSwitchSelect').option('on');
-        select('flipSwitchSelect').option('off');
-        expect(element('.ui-slider-label-a').attr('style')).toBe('width: 0%;');
-        expect(element('.ui-slider-label-b').attr('style')).toBe('width: 100%;');
+    it('mouse over on current value label should highlight', function() {
+        element('.current-value-label').mouseover(); // maybe could use model
+        expect(element('div.current-value-container').class()).toContain('edit-highlight');
+    });
+
+    it('add value to current value label', function() {
+        element('.current-value-label').click();
+        //element('.current-value-label').enter('5');
+        expect(element('a.ui-slider-handle').attr('aria-valuenow')).toBe('5');
+    });
+
+    it('alphabetic entries in the value text field should be prohibited', function() {
+        element('.current-value-label').click();
+        //element('.current-value-label').enter('a');
+        expect(element('a.ui-slider-handle').attr('aria-valuenow')).not.toBe('a');
+    });
+
+    it('numbers that are either too large or too small should "snap" to the closest value when the user finishes editing', function() {
+        element('.current-value-label').click();
+        element('.current-value-label').enter('1000');
+        expect(element('a.ui-slider-handle').attr('aria-valuenow')).toBe('100');
     });
 
 });
